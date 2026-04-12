330cd3311ab8009fe37e303c6dff98403c03c88d
[Tessa] <[tessagerasimova@gmail.com]>
2026-04-12 22:10:32 +0200

    update

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,53 @@
-from flask import Flask
+# what_to_watch/opinions_app.py
+
+from datetime import datetime
+from random import randrange
+
+# Импортировать функцию render_template().
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    quantity = Opinion.query.count()
+    if not quantity:
+        return 'В базе данных записей нет.'
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    # Подключить шаблон opinion.html.
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add')
+def add_opinion_view():
+    # Подключить шаблон add_opinion.html.
+    return render_template('add_opinion.html')
+
+
+# Тут указывается конвертер пути для id.
+@app.route('/opinions/<int:id>')  
+# Параметром указывается имя переменной.
+def opinion_view(id):  
+    # Теперь можно запросить нужный объект по id...
+    opinion = Opinion.query.get_or_404(id)  
+    # ...и передать его в шаблон (шаблон - тот же, что и для главной страницы).
+    return render_template('opinion.html', opinion=opinion) 
+
 
 if __name__ == '__main__':
     app.run()
