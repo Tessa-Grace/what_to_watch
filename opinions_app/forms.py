22c7d635365a57e057dab4abe3765ccf874029ae
[Tessa] <[tessagerasimova@gmail.com]>
2026-05-04 15:42:51 +0200

    upd

---
+++ opinions_app/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf.file import FileAllowed, MultipleFileField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length
+
+
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
+    )
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+    # Добавьте новое поле в форму.
+    images = MultipleFileField()
+    submit = SubmitField('Добавить')
+    images = MultipleFileField(
+        validators=[
+            FileAllowed(
+                # Список разрешенных расширений для файлов.
+                ['jpg', 'jpeg', 'png', 'gif', 'bmp'], 
+                # Сообщение, в случае если расширение не совпадает.
+                message=(
+                    'Выберите файлы с расширением '
+                    '.jpg, .jpeg, .png, .gif или .bmp'
+                )
+            )
+        ]
+    ) 
